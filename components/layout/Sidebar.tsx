ba33bc5b6b509904311c1c65f3e4f3ac7445acb1
mimaralojamentos-oss <mimaralojamentos@gmail.com>
2026-05-26 18:46:06 +0100

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -40,7 +40,7 @@ export default function Sidebar() {
     <aside className="w-64 bg-white border-r border-gray-100 min-h-screen flex flex-col">
       <div className="p-6 border-b border-gray-100">
         <div className="flex items-center gap-3">
-          <div className="w-9 h-9 bg-emerald-600 rounded-lg flex items-center justify-center">
+          <div className="w-9 h-9 bg-blue-600 rounded-lg flex items-center justify-center">
             <Home className="w-5 h-5 text-white" />
           </div>
           <div>
